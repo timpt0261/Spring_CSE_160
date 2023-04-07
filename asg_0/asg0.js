c9f694a7d2b9b1833a0a70a7e173fb71423ce9a7
timpt0261 <rchavez0215@gmail.com>
2023-04-07 15:02:38 -0700

    Able to draw operations

--- asg_0/asg0.js
+++ asg_0/asg0.js
@@ -9,6 +9,7 @@ function main() {
     ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set a black color
     ctx.fillRect(0, 0, 400, 400); // Fill a rectangle with the color
    handleDrawEvent();
+   handleDrawOperationEvent()
 }
 
 function drawVector(v=new Vector3([0,0,0]), color="")
@@ -59,5 +60,66 @@ function handleDrawEvent()
         drawVector(v1, "red");
         drawVector(v2,"blue");
     }
-    
+}
+
+function handleDrawOperationEvent()
+{
+    var canvas = document.getElementById('example');
+    var ctx = canvas.getContext('2d');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element');
+        return;
+    }
+    // Clear canvas
+    const draw = document.getElementById("draw_op");
+    draw.onclick = () => {
+        console.log("Drawing");
+        ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set a black color
+        ctx.fillRect(0, 0, 400, 400); // Fill a rectangle with the color
+        console.log("Clearing Canvas")
+
+        // Read values 
+        const x1_value = document.getElementById("x1-coor");
+        const y1_value = document.getElementById("y1-coor");
+
+        const x2_value = document.getElementById("x2-coor");
+        const y2_value = document.getElementById("y2-coor");
+
+        const operation = document.getElementById("operation");
+        const scaler = document.getElementById("scaler");
+
+        let v1 = new Vector3([x1_value.value, y1_value.value, 0]);
+        let v2 = new Vector3([x2_value.value, y2_value.value, 0]);
+        let v3;
+
+        // Draw vectors
+        drawVector(v1, "red");
+        drawVector(v2, "blue");
+        
+
+        switch(operation.value){
+            case "add":
+                v3 = v1.add(v2);
+                drawVector(v3, "green"); 
+                break;
+            case "sub":
+                v3 = v1.sub(v2);
+                drawVector(v3, "green");
+                break;
+            case "mul": 
+                v3 = v1.mul(scaler.value);
+                drawVector(v3,"green");
+
+                v3 = v2.mul(scaler.value);
+                drawVector(v3, "green");
+                break;
+            case "div":
+                v3 = v1.div(scaler.value);
+                drawVector(v3, "green");
+                
+                v3 = v2.div(scaler.value);
+                drawVector(v3, "green");
+                break;
+        }
+    }
 }
