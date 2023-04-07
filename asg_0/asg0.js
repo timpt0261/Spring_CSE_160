4e8789a0c0a671f4f697c66054ab55c88898897b
timpt0261 <rchavez0215@gmail.com>
2023-04-06 20:39:24 -0700

    Adding Interface

--- asg_0/asg0.js
+++ asg_0/asg0.js
@@ -1,7 +1,6 @@
 // DrawRectangle.js
 function main() {
-    let v1 = new Vector3([2.25, 2.25,0]);
-    drawVector(v1,"red");
+   handleDrawEvent();
 }
 
 function drawVector(v=new Vector3([0,0,0]), color="")
@@ -21,4 +20,26 @@ function drawVector(v=new Vector3([0,0,0]), color="")
     ctx.moveTo(canvas.width/2, canvas.height/2);
     ctx.lineTo(center + v.elements[0] * 20,center-v.elements[1] *20);
     ctx.stroke();
+}
+
+function handleDrawEvent()
+{
+    var canvas = document.getElementById('example');
+    var ctx = canvas.getContext('2d');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element');
+        return;
+    }
+    // Clear canvas
+    const clear = document.getElementById("clear");
+    clear.onclick = () => {
+        ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set a black color
+        ctx.fillRect(0, 0, 400, 400); // Fill a rectangle with the color
+        console.log("Clearing Canvas")
+    }
+    // Read values 
+    let v = new Vector3([0,0,0]);
+
+    // Draw vector
+    drawVector(v,"red");
 }
