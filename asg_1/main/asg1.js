adb83801adee2caa56137e3d986d915b5de5a91d
timpt0261 <rchavez0215@gmail.com>
2023-04-10 16:51:27 -0700

    Added Files and Basic UI in asg1

---
+++ asg_1/main/asg1.js
@@ -0,0 +1,40 @@
+// HelloPoint1.js (c) 2012 matsuda
+// Vertex shader program
+var VSHADER_SOURCE = `
+    void main() {
+      gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+      gl_PointSize = 10.0;
+    }`
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+    void main() {
+      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }`
+
+function main() {
+    // Retrieve <canvas> element
+    var canvas = document.getElementById('webgl');
+
+    // Get the rendering context for WebGL
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    // Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+
+    // Specify the color for clearing <canvas>
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Draw a point
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
